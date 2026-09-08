e11ea6e82582ed696ff608c54ba94e80eb3a1a49
CharlesChristianPilapil <charleschristian.pilapil@gmail.com>
2026-09-08 23:43:10 +0800

    added custom deploy action

--- .github/actions/deploy-s3-javascript/main.js
+++ .github/actions/deploy-s3-javascript/main.js
@@ -1,6 +1,15 @@
 import * as core from "@actions/core";
+// import * as github from "@actions/github";
+import * as exec from "@actions/exec";
 
 function run() {
+    const bucket = core.getInput("bucket", { required: true });
+    const bucketRegion = core.getInput("bucket-region", { required: true });
+    const distFolder = core.getInput("dist-folder", { required: true });
+
+    const s3URI = `s3://${bucket}`;
+    exec.exec(`aws s3 sync ${distFolder} ${s3URI} --region ${bucketRegion}`);
+
     core.notice("Hello from my custom JavaScript action!");
 }
 
