27964679f11d7c8fb108917d512c7340fc0d2ed6
CharlesChristianPilapil <charleschristian.pilapil@gmail.com>
2026-09-09 00:23:02 +0800

    added output

--- .github/actions/deploy-s3-javascript/main.js
+++ .github/actions/deploy-s3-javascript/main.js
@@ -10,7 +10,8 @@ function run() {
     const s3URI = `s3://${bucket}`;
     exec.exec(`aws s3 sync ${distFolder} ${s3URI} --region ${bucketRegion}`);
 
-    core.notice("Hello from my custom JavaScript action!");
+    const websiteUrl = `http://${bucket}.s3-website-${bucketRegion}.amazonaws.com`;
+    core.setOutput("website-url", websiteUrl);
 }
 
 run();
